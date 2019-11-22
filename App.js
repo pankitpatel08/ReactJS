fc4e90c5fcb87e8b792658f607d302fd22d8b78c
Pankit Patel <57940943+pankitpatel08@users.noreply.github.com>
2019-11-22 17:31:23 +1300

    src files

--- App.js
+++ App.js
@@ -5,14 +5,50 @@ import Greet from './Components/Greet'
 import Welcome from './Components/Welcome'
 // import { Greet } from './Components/Greet'
 import Hello from './Components/Hello'
+import Message from './Components/Message'
+import Counter from './Components/Counter'
+import FunctionClick from './Components/FunctionClick'
+import ClassClick from './Components/ClassClick'
+import EventBind from './Components/EventBind'
+import ParentComponent from './Components/ParentComponent'
+import UserGreeting from './Components/UserGreeting'
+import NameList  from './Components/NameList'
+import StyleSheet from './Components/StyleSheet'
+import Inline from './Components/Inline'
+import  './appStyles.css'
+import styles from './appStyles.module.css'
+import Form from './Components/Form'
+
+
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <Welcome /> */}
-      <Hello />
+      <Form />
+      {/* <h1 className='error'>Error</h1> */}
+      {/* <h1 className={styles.success}>Success</h1> */}
+      {/* <Inline /> */}
+      {/* <StyleSheet primary={true}/> */}
+      {/* <NameList /> */}
+      {/* <UserGreeting /> */}
+      {/* <ParentComponent /> */}
+      {/* <Greet name='Pankit' designation='Software Developer' >
+        <p>He is responsible for development of React Code</p>
+      </Greet> */}
+      {/* <Greet name='Uma' designation='Software Tester' >
+        <button>Test</button>
+      </Greet> */}
+      {/* <Greet name='Sanket' designation='Project Manager' /> */}
+      {/* <Welcome name='Pankit' designation='Software Developer'/> */}
+      {/* <Welcome name='Uma' designation='Software Tester'/> */}
+      {/* <Welcome name='Sanket' designation='Project Manager' /> */}
+      {/* <FunctionClick /> */}
+      {/* <ClassClick /> */}
+      {/* <EventBind /> */}
+      {/* <Message /> */}
+      {/* <Counter addValue="4"></Counter> */}
+      {/* <Hello /> */}
 
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
