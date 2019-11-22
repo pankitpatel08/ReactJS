aee16c3e9e2ec7f3572871252fc1f50ecae9d026
Pankit Patel <57940943+pankitpatel08@users.noreply.github.com>
2019-11-22 17:30:48 +1300

    Add files via upload

--- Form.js
+++ Form.js
@@ -6,7 +6,9 @@ class Form extends Component {
         super(props)
 
         this.state = {
-            username: ''
+            username: '',
+            comments: '',
+            topic: 'React'
         }
     }
 
@@ -16,15 +18,51 @@ class Form extends Component {
         })
     }
 
+    handleCommentsChange = (event) => {
+        this.setState({
+            comments: event.target.value
+        })
+    }
+
+    handleTopicChange = (event) => {
+        this.setState({
+            topic: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+        event.preventDefault()
+        // Page not reload after button click
+    }
+
     render() {
         return (
-            <div>
-                <label>Username</label>
-                <input
-                    type='text'
-                    value={this.state.username}
-                    onChange={this.handleUsernameChange} />
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>Username</label>
+                    <input
+                        type='text'
+                        value={this.state.username}
+                        onChange={this.handleUsernameChange} />
+                </div>
+                <div>
+                    <label>Comments</label>
+                    <textarea
+                        value={this.state.comments}
+                        onChange={this.handleCommentsChange} />
+                </div>
+                <div>
+                    <label>Topic</label>
+                    <select value={this.state.topic} onChange={this.handleTopicChange}>
+                        <option value='react'>React</option>
+                        <option value='angular'>Angular</option>
+                        <option value='vue'>Vue</option>
+                    </select>
+                </div>
+                <button type='submit'>Submit</button> 
+                {/* On enter key press handle event = type 'submit'  */}
+            </form>
         )
     }
 }
